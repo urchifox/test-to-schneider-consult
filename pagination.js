276540401bcc7a3120603b6c02cfbf89e3a8cbe6
urchifox <126313096+urchifox@users.noreply.github.com>
2024-03-27 21:59:05 +0400

    update pagination script

---
+++ pagination.js
@@ -0,0 +1,36 @@
+const TARGET_DAY = 1;
+
+const root = document.querySelector('.pagination');
+const elements = root.querySelectorAll('.links__item');
+const today = new Date();
+
+const getClosestDay = (date, target) => {
+    const initialDate = date.getDate();
+    const initialDay = date.getDay();
+    let daysUntilGoalDay = target - initialDay;
+
+    if (daysUntilGoalDay < 0) {
+        daysUntilGoalDay +=7;
+    }
+
+    const closestDay = new Date(date);
+    
+    return new Date(closestDay.setDate(initialDate + daysUntilGoalDay));
+}
+
+const formatDate = (date) => {
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear() % 100;
+
+    return `${day}.${month}.${year}`;
+}
+
+const init = () => {
+    document.addEventListener('DOMContentLoaded', () => {
+        elements.forEach(element => {element.textContent *= 2});
+        elements[0].textContent += ` ${formatDate(getClosestDay(today, TARGET_DAY))}`;
+    });
+}
+
+export {init}
--- test-task-js.js
+++ test-task-js.js
@@ -1,3 +1,5 @@
 import {init as initCollectionTitle} from "./collection-title.js";
+import {init as initPagination} from "./pagination.js";
 
 initCollectionTitle();
+initPagination();
